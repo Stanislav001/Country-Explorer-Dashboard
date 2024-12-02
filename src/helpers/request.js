0c8798753dc762216502655b994e89e03b46146e
Stanislav001 <stanislav.stanchev@netea.bg>
2024-12-02 11:25:51 +0200

    Change API url

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -5,8 +5,8 @@ import axios from "axios";
 axios.defaults.httpAgent = { rejectUnauthorized: false };
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5003/api",
-//   baseURL: "https://travel-api-tau.vercel.app/api",
+  // baseURL: "http://localhost:5003/api",
+  baseURL: "https://travel-api-tau.vercel.app/api",
 });
 
 axiosInstance.interceptors.response.use(
